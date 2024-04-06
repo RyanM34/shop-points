0cba3527c8c650f26f4f374c88ea79525cd8a0c5
RyanM34 <meihaofu@outlook.com>
2024-04-06 14:03:07 -0700

    hfm, feat: update sql

--- docs/shop_points.sql
+++ docs/shop_points.sql
@@ -1,24 +1,13 @@
-DROP TABLE IF EXISTS customer;
-
-CREATE TABLE customer
-(
-    customer_id bigint AUTO_INCREMENT PRIMARY KEY,
-    name        varchar(64) DEFAULT NULL
-);
-
-INSERT INTO customer (customer_id, name)
-VALUES (1, 'test');
-
 DROP TABLE IF EXISTS transaction;
 
 CREATE TABLE transaction (
                              transaction_id bigint AUTO_INCREMENT PRIMARY KEY,
                              customer_id bigint NOT NULL,
                              amount decimal(10, 2) NOT NULL,
-                             created_at datetime DEFAULT NULL
+                             issued_at datetime DEFAULT NULL
 );
 
-INSERT INTO transaction (customer_id, amount, created_at)
-VALUES (1, 120.00, '2024-04-05 12:35:22'),
-       (1, 120.00, '2024-04-05 19:43:32'),
-       (1, 120.00, '2024-04-05 19:48:30');
+INSERT INTO transaction (customer_id, amount, issued_at)
+VALUES (1, 40.00, '2024-01-05 12:35:22'),
+       (1, 60.00, '2024-02-05 19:43:32'),
+       (1, 120.00, '2024-03-05 19:48:30');
